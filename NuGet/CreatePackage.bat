58aa018cca72dd48f1e8be29263b32cdf8b25b0e
Davide Benvegnù <davide.benvegnu@outlook.com>
2017-03-03 17:49:56 +0800

    Added the NuGet definitions and tools

---
+++ NuGet/CreatePackage.bat
@@ -0,0 +1,1 @@
+NuGet.exe pack ./Package/DBTek.BugGuardian.AspNetCore.nuspec -BasePath ./Package
---
+++ NuGet/Package/tools/install.ps1
@@ -0,0 +1,15 @@
+param($installPath, $toolsPath, $package, $project)
+
+# open DBTek.BugGuardian.MVC splash page on package install
+
+try
+{
+  $url = "https://github.com/n3wt0n/BugGuardian.AspNetCore/wiki/Configuration"
+  $dte2 = Get-Interface $dte ([EnvDTE80.DTE2])
+  $dte2.ItemOperations.Navigate($url) | Out-Null      
+}
+catch
+{
+  # stop potential errors from bubbling up
+  # worst case the splash page won't open  
+}
