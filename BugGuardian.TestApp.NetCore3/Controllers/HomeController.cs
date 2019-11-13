542bbee6e58134c386acb1d9963e805c3d6a2765
Davide Benvegnù <davide.benvegnu@outlook.com>
2019-11-13 16:47:00 +0800

    Added Test Web App using .NetCore 3

---
+++ BugGuardian.TestApp.NetCore3/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BugGuardian.TestApp.NetCore3.Models;
+
+namespace BugGuardian.TestApp.NetCore3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int i = 0;
+            int j = 22;
+
+            var result = j / i; //ERROR
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
